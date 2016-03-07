3eb0c2d170d198bc49b40b64e5128d147b13de4a
Alex Nitta <a.j.nitta@gmail.com>
2016-03-07 12:33:13 -0800

    Implement Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,23 @@
 var Bee = function() {
+
+  // call the Grub superclass
+  Grub.call(this);
+
+  // set the prototype
+  Object.setPrototypeOf(this, Grub.prototype);
+  // set the constructor
+  // this.constructor = Grub;
+  
+  // an age property that is set to 5
+  this.age = 5;
+  
+  // a color property that is set to yellow
+  this.color = 'yellow';
+  
+  // a food property that is inherited from grub
+  // an eat method that is inherited from grub
+  
+  // a job property that is set to keep on growing
+  this.job = 'keep on growing';
 };
 
