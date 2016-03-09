63d8dac69c5d45ab305ad1f26e80dcd7324b825b
Alex Nitta <a.j.nitta@gmail.com>
2016-03-08 18:52:27 -0800

    Implement ForagerBee class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,23 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+};
+
+ForagerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
